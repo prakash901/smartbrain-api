e9ad45ca02cb10f21483512c85fc3c8f85702e2c
prakash901 <prakashtiwari901@gmail.com>
2020-08-16 00:18:54 +0545

    version one complete

---
+++ controllers/image.js
@@ -0,0 +1,36 @@
+const clarifai =  require('Clarifai');
+const { json } = require('body-parser');
+
+//api key
+const app = new Clarifai.App({
+    apiKey: 'ee1a177cd2434cc2bd6c8a1a469fa4f3'
+   });
+
+   const handleApiCall = (req, res) => {
+
+       app.models
+       .predict(Clarifai.FACE_DETECT_MODEL, 
+       req.body.input)
+       .then(data => {
+           res.json(data);
+       })
+   .catch(err => res.status(400).json('unable to work with API'))
+
+   }
+
+const handleImage =  (req, res, db) => {
+    const { id } = req.body;
+  db('users').where('id', '=', id)
+  .increment('entries', 1)
+  .returning('entries')
+  .then(entries => {
+      res.json(entries[0]);
+  })
+  .catch(err => res.status(400).json('unable to get entries'))
+
+}
+
+module.exports = {
+    handleImage,
+    handleApiCall
+}
